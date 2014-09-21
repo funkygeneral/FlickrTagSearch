958b6abd2f30198b0ce9f207bb435dfb164ae363
Nicolai Lyden <nlyden@googlemail.com>
2014-09-21 17:26:50 +0200

    Changed the layout and implemented the EditText and Search Button

--- src/com/funkygeneral/flickrtagsearch/Main.java
+++ src/com/funkygeneral/flickrtagsearch/Main.java
@@ -6,19 +6,40 @@ import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.os.Build;
 
 public class Main extends ActionBarActivity {
+	
+	String TAG = "Main Activity";
 
+	EditText text_input;
+	Button searchButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		text_input = (EditText) findViewById(R.id.text_input);
+		searchButton = (Button) findViewById(R.id.search_button);
+		searchButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String tagWord = text_input.getText().toString();
+				Log.d(TAG, tagWord);
+			}
+			
+		});
 	}
 
 	@Override
